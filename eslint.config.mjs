7f02ae6c6a2986b97f29f1e4f92fe9f8fca947d5
Francesco <francesco@xevolab.com>
2025-03-25 00:08:47 +0100

    Typescript implementation

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+/*
+ * Author    : Francesco
+ * Created at: 2024-02-03 11:29
+ * Edited by : Francesco
+ * Edited at : 2025-03-24 23:50
+ *
+ * Copyright (c) 2024 Xevolab S.R.L.
+ */
+
+import backend from "@xevolab/eslint-config/backend-ts";
+
+export default backend;
--- src/index.ts
+++ src/index.ts
@@ -2,37 +2,46 @@
  * Author    : Francesco
  * Created at: 2023-03-16 20:30
  * Edited by : Francesco
- * Edited at : 2023-04-25 16:26
+ * Edited at : 2025-03-25 00:07
  *
  * Copyright (c) 2023 Xevolab S.R.L.
  */
 
 const permRegex = /^(\+|-)?(([A-Za-z0-9]+)|(\*))@[a-zA-Z0-9]+(((:[a-zA-Z0-9-_/]*)*(:[a-zA-Z0-9-_/]+)$)|$)/;
 
+type AppName = string;
+type ResourceName = string;
+type PermissionName = string;
+type PermissionValue = "+" | "-";
+type InternalPermissionValue = PermissionValue | `++` | "--";
+type PermissionsTree<T = PermissionValue> = Record<AppName,
+	Record<
+		ResourceName,
+		Record<PermissionName, T>
+	>
+>;
+
+type PermissionString = `${PermissionValue | ""}${PermissionName}@${AppName}${ResourceName | ""}`;
+
 /**
  * The parsePermissions function takes the permission blocks, which are
  * lists of permissions strings, and converts them into a tree-like
  * structure that can be used to check whether a user has a certain
  * permission.
- * @param  {[[string]]} permBlocks  A list of lists of permissions strings
- *                                  granted to the user sorted from the
- *                                  least important to the most important.
- * @return {object}                 An object containing the permissions
- *                                  in a tree-like structure.
+ * @param  permBlocks  A list of lists of permissions strings granted to the user sorted from the
+ *                     least important to the most important.
+ * @return             An object containing the permissions in a tree-like structure.
  */
-export const parsePermissions = (permBlocks = []) => {
+export const parsePermissions = (permBlocks: PermissionString[][] = []): PermissionsTree => {
+	const apps: PermissionsTree<InternalPermissionValue> = {};
 
-	let apps = {};
-
-	function processPerms(perms, overwrite = false) {
-
+	function processPerms(perms: PermissionString[], overwrite = false) {
 		// This array keeps track of the permissions that have been overridden
 		// by this loop and are now in a `++` or `--` state, which need to be
 		// converted back to a single `+` or `-`
-		let permsToFinalize = [];
+		const permsToFinalize = [];
 
-		for (let p of perms) {
-
+		for (const p of perms) {
 			/*
 			  Permissions follow the structure:
 
@@ -46,24 +55,24 @@ export const parsePermissions = (permBlocks = []) => {
 			if (!permRegex.test(p)) continue;
 
 			// The action character can be omitted, implying a +
-			let permAct = p.slice(0, 1) !== "-" ? "+" : "-";
+			const permAct: PermissionValue | ("++" | "--") = p.slice(0, 1) !== "-" ? "+" : "-";
 
-			let perm = p.split("@")[0]
+			let perm = p.split("@")[0];
 			if (p.slice(0, 1) === "+" || p.slice(0, 1) === "-") perm = perm.slice(1);
 
 			// The permission string is split into its components
-			let split = p.split("@")[1].split(":");
+			const split = p.split("@")[1].split(":");
 
 			// Separating the app and resource portion of the string
-			let app = split[0];
-			let resource = split.slice(1).join(":");
+			const app = split[0];
+			const resource = split.slice(1).join(":");
 
 			// Update the app object by adding the different permissions
 			// in a tree
 			if (apps[app]) {
 				// If the app exists, but not the resource, create that and move on
 				if (!apps[app][resource]) {
-					apps[app][resource] = { [perm]: permAct + (overwrite ? permAct : "") };
+					apps[app][resource] = { [perm]: permAct + (overwrite ? permAct : "") as InternalPermissionValue };
 					if (overwrite) permsToFinalize.push([app, resource, perm]);
 					continue;
 				}
@@ -84,40 +93,36 @@ export const parsePermissions = (permBlocks = []) => {
 					if (apps[app][resource][perm] === "++") continue;
 
 					// Otherwise update the existing value to give it a positive value
-					apps[app][resource][perm] = permAct + (overwrite ? permAct : "");
-					if (overwrite) permsToFinalize.push([app, resource, perm])
+					apps[app][resource][perm] = permAct + (overwrite ? permAct : "") as InternalPermissionValue;
+					if (overwrite) permsToFinalize.push([app, resource, perm]);
 					continue;
 				}
 
 				// If the permission is still not applied at this point, it must be a new one
-				if (!apps[app][resource][perm]) apps[app][resource][perm] = permAct
+				if (!apps[app][resource][perm]) apps[app][resource][perm] = permAct;
 			}
 			else {
 				apps[app] = {
 					[resource]: { [perm]: permAct },
-					...(resource !== "" ? { "": {} } : {})
-				}
+					...(resource !== "" ? { "": {} } : {}),
+				};
 			}
-
 		}
 
 		// Finalize the permissions that have been overridden by removing the
 		// extra `+` or `-` from the permission string
-		for (let i of permsToFinalize) {
-			apps[i[0]][i[1]][i[2]] = apps[i[0]][i[1]][i[2]].slice(0, 1);
-		}
-
+		// eslint-disable-next-line max-len
+		for (const i of permsToFinalize) apps[i[0]][i[1]][i[2]] = apps[i[0]][i[1]][i[2]].slice(0, 1) as PermissionValue;
 	}
 
 	/*
 	  Create the tree that describes the permissions assigned to the user by
 	  going through every block of permissions and processing them.
 	 */
-	for (const i in permBlocks) processPerms(permBlocks[i], i > 0);
+	for (let i = 0; i < permBlocks.length; i++) processPerms(permBlocks[i], i > 0);
 
-	return apps
-
-}
+	return apps as PermissionsTree;
+};
 
 /**
  * stringifyPermissions takes the tree-like permissions structure and
@@ -125,35 +130,31 @@ export const parsePermissions = (permBlocks = []) => {
  * following the grammar defined in the documentation.
  * In essence, it re-creates the original lines from the database after
  * they are processed taking inheritance and priority into account
- * @param  {object}    p          tree-like structure
- * @return {[string]}             permissions as array of strings
+ * @param  p          tree-like structure
+ * @return            permissions as array of strings
  */
-export const stringifyPermissions = (p) => {
-	let permissionStrings = [];
+export const stringifyPermissions = (p: PermissionsTree): PermissionString[] => {
+	const permissionStrings: PermissionString[] = [];
 
 	// For each app
-	for (let app of Object.keys(p)) {
+	for (const app of Object.keys(p)) {
 		// There are multiple resources
-		for (let res of Object.keys(p[app])) {
+		for (const res of Object.keys(p[app])) {
 			// Each of which has permissions
-			for (let perm of Object.keys(p[app][res])) {
-				permissionStrings.push(p[app][res][perm] + perm + "@" + app + (res !== "" ? ":" + res : ""))
-			}
+			for (const perm of Object.keys(p[app][res])) permissionStrings.push(`${p[app][res][perm] + perm}@${app}${res !== "" ? `:${res}` : ""}`);
 		}
 	}
 
 	return permissionStrings;
-}
+};
 
 /**
  * validatePermission checks that a passed permission string is valid when
  * checked against the
- * @param   {string}  perm  The permission in a string form
- * @return  {boolean}       Whether the permission string is valid
+ * @param   perm  The permission in a string form
+ * @return       Whether the permission string is valid
  */
-export const validatePermission = (perm) => {
-	return permRegex.test(perm);
-}
+export const validatePermission = (perm: PermissionString): boolean => permRegex.test(perm);
 
 /**
  * Authorize is used to decide whether a user is allowed to perform
@@ -161,47 +162,43 @@ export const validatePermission = (perm) => {
  * he/she has the permissions defined in the `granted` object.
  * The `requested` should be the most accurate representation of the action
  * that is being performed.
- * @param  {object} granted      The permission object that rapresents
- *                               what the user is allowed to do
- * @param  {string} requested    The permissions required to perform
- *                               the action
- * @param  {boolean} simpleMode  Whether to return a simple boolean or
- * 									   a more detailed object
- * @return {object}
+ * @param granted     The permission object that rapresents what the user is allowed to do
+ * @param requested   The permissions required to perform the action
+ * @param simpleMode  Whether to return a simple boolean or a more detailed object
  */
-export const authorize = (granted, requested, simpleMode = true) => {
-
-	function result(e) {
+export const authorize = (
+	granted: PermissionsTree,
+	requested: PermissionString,
+	simpleMode = true,
+) => {
+	function result(e: { ok?: boolean, authorized: boolean, message?: string, error?: string }) {
 		// console.log("Auth result debug: ", e);
 		if (simpleMode) return (typeof e.authorized === "boolean") ? e.authorized : false;
 
 		return {
 			ok: true,
-			authorized: true,
-			...e
-		}
+			...e,
+		};
 	}
 
 	// Validating the requested permissions
-	if (!permRegex.test(requested)) {
-		return result({ ok: false, authorized: false, error: "Invalid requested permission" })
-	}
+	if (!permRegex.test(requested)) return result({ ok: false, authorized: false, error: "Invalid requested permission" });
+
 
 	/* Splitting the permission in its components */
 
 	// The action character can be omitted, implying a +
-	let perm = requested.split("@")[0]
+	let perm = requested.split("@")[0];
 	if (requested.slice(0, 1) === "+" || requested.slice(0, 1) === "-") perm = perm.slice(1);
 
 	// The permission string is split into its components
-	let split = requested.split("@")[1].split(":");
-	let app = split[0];
+	const split = requested.split("@")[1].split(":");
+	const app = split[0];
 
 	/* Checking if the requested permission is granted to the user */
 
 	// Making sure the app itself is present
-	if (!granted[app])
-		return result({ authorized: false, message: "The user does not have access to the '" + app + "' app" })
+	if (!granted[app]) return result({ authorized: false, message: `The user does not have access to the '${app}' app` });
 
 	// Then going through every component of the resource one by one in
 	// reverse to check if the permission in grated at a more general
@@ -212,7 +209,6 @@ export const authorize = (granted, requested, simpleMode = true) => {
 	// blocked as the negative permission is more specific.
 
 	for (let i = split.length; i > 0; i--) {
-
 		// console.log("--> " + split.slice(1, i).join(":"));
 		// Each component of the resouce element can be omitted to allow for
 		// whatever value to be placed there.
@@ -221,15 +217,15 @@ export const authorize = (granted, requested, simpleMode = true) => {
 		// element and check if a permission on that resource is present.
 
 		for (let j = i; j > 0; j--) {
-
-			let resource = split.slice(1, i);
+			let resourceComponents = split.slice(1, i);
 
 			// When the position to remove is the last element, slice the array
 			// so that it doesn't have a trailing ':'
-			if (j === resource.length) resource = resource.slice(0, resource.length - 1)
+			// eslint-disable-next-line max-len
+			if (j === resourceComponents.length) resourceComponents = resourceComponents.slice(0, resourceComponents.length - 1);
 			// If the position is before the last, just leave "::"
-			if (j < resource.length) resource[j - 1] = ""
-			resource = resource.join(":");
+			if (j < resourceComponents.length) resourceComponents[j - 1] = "";
+			const resource = resourceComponents.join(":");
 
 			// console.log("    " + perm + "@" + app + ":" + resource);
 
@@ -237,27 +233,22 @@ export const authorize = (granted, requested, simpleMode = true) => {
 			if (granted[app][resource]) {
 				// If a match is found and the permission is present, this is the
 				// result so that a specific '-' is more powerful than a generic '+'
-				if (granted[app][resource][perm] === "-")
-					return result({ authorized: false, message: `The permission -${perm}@${app}:${resource} blocks access` })
-				if (granted[app][resource]["*"] === "-")
-					return result({ authorized: false, message: `The permission -*@${app}:${resource} blocks access` })
+				if (granted[app][resource][perm] === "-") return result({ authorized: false, message: `The permission -${perm}@${app}:${resource} blocks access` });
+				if (granted[app][resource]["*"] === "-") return result({ authorized: false, message: `The permission -*@${app}:${resource} blocks access` });
 
 				// If a '+' is specifically granted to this perm or a general '*',
 				// authorize the request
-				if (granted[app][resource][perm] === "+")
-					return result({ authorized: true, message: `The permission +${perm}@${app}:${resource} grants access` })
-				if (granted[app][resource]["*"] === "+")
-					return result({ authorized: true, message: `The permission +*@${app}:${resource} grants access` })
+				if (granted[app][resource][perm] === "+") return result({ authorized: true, message: `The permission +${perm}@${app}:${resource} grants access` });
+				if (granted[app][resource]["*"] === "+") return result({ authorized: true, message: `The permission +*@${app}:${resource} grants access` });
 			}
 		}
 	}
 
-	/*// Last chance is to check for the '*' resource wildcard
+	/* // Last chance is to check for the '*' resource wildcard
 	if (granted[app]["*"] && granted[app]["*"][perm] === "+")
 	  return result({message: `The permission +${perm}@${app} is set to grant access`})
 	if (granted[app]["*"] && granted[app]["*"]["*"] === "+")
-	  return result({message: `The permission +*@${app} is set to grant access`})*/
+	  return result({message: `The permission +*@${app} is set to grant access`}) */
 
-	return result({ authorized: false, message: `No authorization was found for this resource` })
-
-}
+	return result({ authorized: false, message: `No authorization was found for this resource` });
+};
